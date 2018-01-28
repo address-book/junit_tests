15723063afb8e819a05435912b2d0d29dbcda4d8
Titus Fortner <titusfortner@gmail.com>
2018-01-28 15:24:37 -0800

    write element location test

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -3,7 +3,9 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static junit.framework.TestCase.assertEquals;
 
@@ -23,10 +25,17 @@ public class SeleniumExamples extends Base {
     }
 
     @Test
-    public void navigateBrowser() {
+    public void locateElement() {
         browser.get("http://address-book-example.herokuapp.com");
 
-        assertEquals("AddressBook", browser.getTitle());
+        WebElement linkText = browser.findElement(By.linkText("Sign in"));
+        WebElement partialLinkText = browser.findElement(By.partialLinkText("Sign"));
+        WebElement css = browser.findElement(By.cssSelector("a[data-test=sign-in]"));
+        WebElement xpath = browser.findElement(By.xpath("//a[@data-test='sign-in']"));
+
+        assertEquals(linkText, partialLinkText);
+        assertEquals(partialLinkText, css);
+        assertEquals(css, xpath);
     }
 
 }
