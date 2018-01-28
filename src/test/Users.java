d8dbf1e858a639afd6a09a1c985ffbed547c86a7
Titus Fortner <titusfortner@gmail.com>
2018-01-28 15:54:38 -0800

    fix test with an explicit wait

--- src/test/Users.java
+++ src/test/Users.java
@@ -2,40 +2,26 @@ package test;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.function.Function;
-
-import static java.util.concurrent.TimeUnit.MICROSECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static junit.framework.TestCase.assertEquals;
 
 public class Users extends Base {
 
     @Test
-    public void login() throws InterruptedException {
+    public void login() {
         browser.get("http://address-book-example.herokuapp.com");
 
         browser.findElement(By.cssSelector("a[data-test=sign-in]")).click();
 
         String email = "techwell@example.com";
+        WebDriverWait wait = new WebDriverWait(browser, 10);
 
-        Wait fluentWait = new FluentWait(browser)
-                .withTimeout(5, SECONDS)
-                .pollingEvery(500, MICROSECONDS)
-                .ignoring(NoSuchElementException.class);
+        wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.id("session_email"))).sendKeys(email);
 
-        WebElement emailElement = (WebElement) fluentWait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver browser) {
-                return browser.findElement(By.id("session_email"));
-            }
-        });
-
-        emailElement.sendKeys(email);
         browser.findElement(By.id("session_password")).sendKeys("password");
         browser.findElement(By.name("commit")).click();
 
