863cb9ba815122bfa85a52380a12e8e9a5fd7c56
Titus Fortner <titusfortner@gmail.com>
2018-01-29 14:43:39 -0600

    implement sign up/out/in tests (test data problem)

--- src/test/Users.java
+++ src/test/Users.java
@@ -6,28 +6,53 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class Users extends Base {
 
     @Test
-    public void login() {
+    public void signUpLogOutLogIn() {
         browser.get("http://address-book-example.herokuapp.com");
+
+        // Sign Up
 
         browser.findElement(By.cssSelector("a[data-test=sign-in]")).click();
 
-        String email = "techwell@example.com";
         WebDriverWait wait = new WebDriverWait(browser, 10);
 
         wait.until(
                 ExpectedConditions.presenceOfElementLocated(
-                        By.id("session_email"))).sendKeys(email);
+                        By.cssSelector("a[data-test=sign-up]"))).click();
 
-        browser.findElement(By.id("session_password")).sendKeys("password");
+        String email = "techwell@example.com";
+        String password = "password";
+
+        wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.id("user_email"))).sendKeys(email);
+
+        browser.findElement(By.id("user_password")).sendKeys(password);
         browser.findElement(By.name("commit")).click();
 
-        String user = browser.findElement(By.cssSelector("span[data-test=current-user]")).getText();
+        String signedUpUser = browser.findElement(By.cssSelector("span[data-test=current-user]")).getText();
 
-        assertEquals(email, user);
+        assertEquals(email, signedUpUser);
+
+        // Log Out
+
+        browser.findElement(By.cssSelector("a[data-test=sign-out]")).click();
+
+        assertTrue(browser.findElements(By.cssSelector("span[data-test=current-user]")).isEmpty());
+
+        // Log In
+
+        browser.findElement(By.id("session_email")).sendKeys(email);
+        browser.findElement(By.id("session_password")).sendKeys(password);
+        browser.findElement(By.name("commit")).click();
+
+        String loggedInUser = browser.findElement(By.cssSelector("span[data-test=current-user]")).getText();
+
+        assertEquals(email, loggedInUser);
     }
 
 }
