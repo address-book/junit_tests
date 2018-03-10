9905a05dea4993ea00c30fee02e5f7a825b6724a
Titus Fortner <titusfortner@gmail.com>
2018-03-10 13:09:54 -0800

    Add Navigation

--- src/test/SeleniumScript.java
+++ src/test/SeleniumScript.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumScript {
 
     @Test
-    public void openBrowser() {
+    public void navigation() {
         // Set location of chromedriver
         System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
 
         // Start session (opens browser)
         WebDriver driver = new ChromeDriver();
+
+        // Navigate
+        driver.get("http://a.testaddressbook.com");
 
         // Quit session (closes browser)
         driver.quit();
