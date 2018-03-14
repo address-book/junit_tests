b5f910d6546509ba22d9d10f08c226465a0e68f2
Titus Fortner <titusfortner@gmail.com>
2018-03-14 12:51:12 -0700

    fix with fluent wait

--- src/test/user/SignIn.java
+++ src/test/user/SignIn.java
@@ -2,8 +2,17 @@ package test.user;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import test.Base;
 
+import java.util.function.Function;
+
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
 public class SignIn extends  Base {
@@ -16,13 +25,20 @@ public class SignIn extends  Base {
         String email = "techwell@example.com";
         String password = "password";
 
-        Thread.sleep(1000);
+        Wait fluentWait = new FluentWait(driver)
+                .withTimeout(5, SECONDS)
+                .pollingEvery(500, MICROSECONDS)
+                .ignoring(NoSuchElementException.class);
 
-        driver.findElement(By.id("session_email")).sendKeys(email);
+        WebElement emailElement = (WebElement) fluentWait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver browser) {
+                return browser.findElement(By.id("session_email"));
+            }
+        });
+
+        emailElement.sendKeys(email);
         driver.findElement(By.id("session_password")).sendKeys(password);
         driver.findElement(By.tagName("form")).submit();
-
-        Thread.sleep(1000);
 
         By currentUser = By.cssSelector("span[data-test=current-user]");
         String user = driver.findElement(currentUser).getText();
