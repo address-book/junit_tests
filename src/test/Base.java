4b11d48c89db32565178436e90fb9a55a6be90f8
Titus Fortner <titusfortner@gmail.com>
2018-03-13 17:18:22 -0700

    Location Exercise

--- src/test/Base.java
+++ src/test/Base.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
 
-    WebDriver driver;
+    protected WebDriver driver;
 
     @Rule
     public TestWatcher watcher = new TestWatcher() {
