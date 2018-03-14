29dc29b0c1f43865a1104966346f180ae855a0d7
Titus Fortner <titusfortner@gmail.com>
2018-03-14 01:08:24 +0100

    Move common code to Super Class

---
+++ src/test/Base.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+}
--- src/test/SeleniumScript.java
+++ src/test/SeleniumScript.java
@@ -2,23 +2,14 @@ package test;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SeleniumScript {
+public class SeleniumScript extends Base{
 
     @Test
     public void signInLink() {
-        // Set location of chromedriver
-        // Note: Windows Users will need to add `.exe` to the end of the file location
-        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
-
-        // Start session (opens browser)
-        WebDriver driver = new ChromeDriver();
-
         // Navigate
         driver.get("http://a.testaddressbook.com");
 
@@ -37,9 +28,6 @@ public class SeleniumScript {
         Boolean result = ("Address Book - Sign In").equals(driver.getTitle());
         String status = result ? "passed" : "failed";
         System.out.println(status);
-
-        // Quit session (closes browser)
-        driver.quit();
     }
 
 }
