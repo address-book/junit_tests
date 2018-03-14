d85fdf0de962183e6b31518f4bcdeddecd0f47cd
Titus Fortner <titusfortner@gmail.com>
2018-03-13 17:08:24 -0700

    Move common code to Super Class

---
+++ src/test/Base.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+
+    WebDriver driver;
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.println(description.getMethodName() + ": Failed");
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println(description.getMethodName() + ": Succeeded");
+        }
+    };
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+}
--- src/test/SeleniumScript.java
+++ src/test/SeleniumScript.java
@@ -1,41 +1,17 @@
 package test;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertEquals;
 
-public class SeleniumScript {
-
-    @Rule
-    public TestWatcher watcher = new TestWatcher() {
-        @Override
-        protected void failed(Throwable e, Description description) {
-            System.out.println(description.getMethodName() + ": Failed");
-        }
-
-        @Override
-        protected void succeeded(Description description) {
-            System.out.println(description.getMethodName() + ": Succeeded");
-        }
-    };
+public class SeleniumScript extends Base {
 
     @Test
     public void signInLink() {
-        // Set location of chromedriver
-        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
-
-        // Start session (opens browser)
-        WebDriver driver = new ChromeDriver();
-
         // Navigate
         driver.get("http://a.testaddressbook.com");
 
@@ -50,9 +26,6 @@ public class SeleniumScript {
 
         // Assert desired condition
         assertEquals("Address Book - Sign In", driver.getTitle());
-
-        // Quit session (closes browser)
-        driver.quit();
     }
 
 }
