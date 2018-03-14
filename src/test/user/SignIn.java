651c0c575cfd47a952c087289fb8c0cad22f7e79
Titus Fortner <titusfortner@gmail.com>
2018-03-14 16:48:35 -0700

    Write the test first

--- src/test/user/SignIn.java
+++ src/test/user/SignIn.java
@@ -1,38 +1,28 @@
 package test.user;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import test.base.Base;
 import test.data.UserData;
 import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class SignIn extends  Base {
+public class SignIn extends Base {
 
     @Test
     public void signIn() {
-        driver.get("http://a.testaddressbook.com");
-        driver.findElement(By.id("sign-in")).click();
+        HomePage homePage = HomePage.visit(driver);
+        homePage.navigateToSignIn();
+
+        SignInPage signInPage = new SignInPage(driver);
 
         HashMap validUser = (HashMap) new UserData().validUser();
         String email = (String) validUser.get("email");
         String password = (String) validUser.get("password");
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.id("session_email"))).sendKeys(email);
+        signInPage.signIn(email, password);
 
-        driver.findElement(By.id("session_password")).sendKeys(password);
-        driver.findElement(By.tagName("form")).submit();
-
-        By currentUser = By.cssSelector("span[data-test=current-user]");
-        String user = driver.findElement(currentUser).getText();
-
-        assertEquals(email, user);
+        assertTrue(homePage.isSignedIn(email));
     }
 
 }
