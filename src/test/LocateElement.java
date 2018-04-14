67ab2fbc1ace06f7db64e25633eb0584fc6637c5
Titus Fortner <titusfortner@gmail.com>
2018-04-14 20:22:33 +0200

    Locate Element by findElement

--- src/test/LocateElement.java
+++ src/test/LocateElement.java
@@ -6,13 +6,22 @@ import org.openqa.selenium.By;
 public class LocateElement extends  Base {
 
     @Test
-    public void locateElementByFindElement() {
+    public void locateSignInLinkByFindElement() {
         driver.get("http://a.testaddressbook.com");
 
+        // ID
         driver.findElement(By.id("sign-in"));
+
+        // CSS Selector
         driver.findElement(By.cssSelector("a[data-test=sign-in]"));
+
+        // Link Text
         driver.findElement(By.linkText("Sign in"));
+
+        // Partial Link Text
         driver.findElement(By.partialLinkText("Sign"));
+
+        // XPath
         driver.findElement(By.xpath("//a[@data-test='sign-in']"));
     }
 
