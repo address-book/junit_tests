48ca8a3cf519bf64157b5ac16d3a5f3b65a1add4
Titus Fortner <titusfortner@gmail.com>
2018-04-15 13:55:43 -0500

    implement with Fluent Wait

--- src/test/SignInTest.java
+++ src/test/SignInTest.java
@@ -2,20 +2,36 @@ package test;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
 
 public class SignInTest extends Base {
 
     @Test
-    public void signInLink() throws InterruptedException {
+    public void signInLink() {
         driver.get("http://a.testaddressbook.com");
         driver.findElement(By.id("sign-in")).click();
 
-        Thread.sleep(2000);
+        Wait fluentWait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
 
-        driver.findElement(By.id("session_email")).
-                sendKeys("user@example.com");
-        driver.findElement(By.id("session_password")).
-                sendKeys("password");
+        WebElement emailElement = (WebElement) fluentWait
+                .until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver browser) {
+                return browser.findElement(By.id("session_email"));
+            }
+        });
+
+        emailElement.sendKeys("user@example.com");
+        driver.findElement(By.id("session_password")).sendKeys("password");
         driver.findElement(By.name("commit")).click();
     }
 
