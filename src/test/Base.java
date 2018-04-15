78851ec267e6361848edeaadc46a425d7a926043
Titus Fortner <titusfortner@gmail.com>
2018-04-15 14:17:28 -0500

    Add Sauce option to Base Test class toggled on ENV variable

--- src/test/Base.java
+++ src/test/Base.java
@@ -4,17 +4,34 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Base {
 
     protected WebDriver driver;
+    private String useSauce = System.getenv("USE_SAUCE");
 
     @Before
     public void setup() throws MalformedURLException {
-        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
-        driver = new ChromeDriver();
+        if (useSauce == null) {
+            System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver");
+            driver = new ChromeDriver();
+        } else {
+            String user = System.getenv("SAUCE_USERNAME");
+            String key = System.getenv("SAUCE_ACCESS_KEY");
+
+            String url = "https://"+user+":"+key+"@ondemand.saucelabs.com:443/wd/hub";
+
+            DesiredCapabilities caps = DesiredCapabilities.chrome();
+            caps.setCapability("platform", "macOS 10.12");
+            caps.setCapability("version", "63.0");
+
+            driver = new RemoteWebDriver(new URL(url), caps);
+        }
     }
 
     @After
