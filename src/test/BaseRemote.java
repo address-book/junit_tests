20a134e4d86fcc112e077c542212d1112c0cac6c
Titus Fortner <titusfortner@gmail.com>
2018-04-15 10:26:05 -0500

    Run Selenium Script with Remote WebDriver

---
+++ src/test/BaseRemote.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class BaseRemote extends Base {
+
+    @Before
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+
+        String URL = "http://localhost:4444/wd/hub";
+        driver = new RemoteWebDriver(new URL(URL), caps);
+    }
+
+}
--- src/test/SeleniumScript.java
+++ src/test/SeleniumScript.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SeleniumScript extends Base{
+public class SeleniumScript extends BaseRemote {
 
     @Test
     public void signInLink() {
