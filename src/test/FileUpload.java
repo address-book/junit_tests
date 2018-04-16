5a5e892497b09bc69dcefaaf2b6bf30a6023b373
Titus Fortner <titusfortner@gmail.com>
2018-04-16 03:13:51 -0500

    show file uploading

---
+++ src/test/FileUpload.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.LocalFileDetector;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUpload extends Base {
+
+    @Test
+    public void uploadFile() throws IOException {
+        // Uncomment for Remote WebDriver
+        // driver.setFileDetector(new LocalFileDetector());
+
+        driver.get("http://the-internet.herokuapp.com/upload");
+
+        WebElement inputElement = driver.findElement(By.id("file-upload"));
+        File file = new File("lib/drivers/chromedriver");
+        inputElement.sendKeys(file.getCanonicalPath());
+    }
+
+}
+
