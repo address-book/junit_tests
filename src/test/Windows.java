83bdd4d3f9165b28ec06444426153bd6e58a08ed
Titus Fortner <titusfortner@gmail.com>
2018-04-16 03:14:25 -0500

    show window handling

---
+++ src/test/Windows.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.Set;
+
+public class Windows extends Base {
+
+    @Test
+    public void windowSwitching() {
+        driver.get("https://google.com");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.open('https://www.example.com');");
+
+        String origWindow = driver.getWindowHandle();
+        Set handles = driver.getWindowHandles();
+        handles.remove(origWindow);
+
+        String nextWindow = String.valueOf(handles.iterator().next());
+
+        // "Example Domain"
+        driver.switchTo().window(nextWindow);
+
+        driver.close();
+
+        // "Google"
+        driver.switchTo().window(origWindow);
+    }
+
+}
+
