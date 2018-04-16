fb781a86745692374f3a845e820d5b1e257d76dd
Titus Fortner <titusfortner@gmail.com>
2018-04-15 21:26:26 -0500

    implement failing Sign Up test leveraging default data

--- src/test/SignUpTest.java
+++ src/test/SignUpTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import test.data.UserData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class SignUpTest extends Base {
@@ -19,6 +22,21 @@ public class SignUpTest extends Base {
         driver.findElement(By.name("commit")).click();
 
         assertEquals("Address Book", driver.getTitle());
+    }
+
+    @Test
+    public void signUpFailBlankPassword() {
+        driver.get("http://a.testaddressbook.com/sign_up");
+
+        Map<String, String> requiredData = new HashMap<String, String>();
+        requiredData.put("password", "");
+        UserData newUser = new UserData(requiredData);
+
+        driver.findElement(By.id("user_email")).sendKeys(newUser.getEmail());
+        driver.findElement(By.id("user_password")).sendKeys(newUser.getPassword());
+        driver.findElement(By.name("commit")).click();
+
+        assertEquals("Address Book - Sign Up", driver.getTitle());
     }
 
 }
