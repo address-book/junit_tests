67c350e66ed666aa6ef9c3e0345d6e39ba97055f
Titus Fortner <titusfortner@gmail.com>
2018-04-30 08:14:34 -0500

    Frames example

---
+++ src/test/Frames.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.Test;
+
+public class Frames extends Base {
+
+    @Test
+    public void frameSwitching() {
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+
+        // "BOTTOM"
+        driver.switchTo().frame(1);
+
+        // Move up one context
+        driver.switchTo().parentFrame();
+
+        // ""
+        driver.switchTo().frame("frame-top");
+
+        // "LEFT"
+        driver.switchTo().frame("frame-left");
+
+        // Move back to top context
+        driver.switchTo().defaultContent();
+    }
+
+}
+
