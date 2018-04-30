c05df7c097953c5a8718bc6718a2d0696a310f8b
Titus Fortner <titusfortner@gmail.com>
2018-04-30 08:08:31 -0500

    Cookie example

---
+++ src/test/Cookies.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+
+import java.util.Date;
+
+public class Cookies extends Base {
+
+    @Test
+    public void cIsForCookie() {
+        driver.get("http://a.testaddressbook.com");
+
+        // Create
+        Cookie cookie = new Cookie.Builder("name", "value")
+                .domain("a.testaddressbook.com")
+                .expiresOn(new Date(2020, 10, 31))
+                .isHttpOnly(true)
+                .isSecure(false)
+                .path("/mypath")
+                .build();
+
+        // Add
+        driver.manage().addCookie(cookie);
+
+        // Read
+        driver.get("http://a.testaddressbook.com/mypath");
+        driver.manage().getCookieNamed("name");
+
+        // Get All
+        driver.manage().getCookies();
+
+        // Delete
+        driver.manage().deleteCookie(cookie);
+    }
+
+}
+
