e113b9448b638d5fd433e399396dfb3d80f878a7
Titus Fortner <titusfortner@gmail.com>
2018-04-30 07:57:02 -0500

    Getting information from a Browser

---
+++ src/test/BrowserInteractions.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Test;
+
+public class BrowserInteractions extends Base {
+
+    @Test
+    public void browserInformation() {
+        driver.get("http://a.testaddressbook.com");
+
+        // "Address Book"
+        driver.getTitle();
+
+        // "http://a.testaddressbook.com/"
+        driver.getCurrentUrl();
+
+        // "<html> ... </html>"
+        driver.getPageSource();
+    }
+}
