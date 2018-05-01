e78761339b770ce677261c3e2635262a103d2700
Titus Fortner <titusfortner@gmail.com>
2018-04-30 20:10:12 -0400

    Answers for Exercise to implement Sign In Page

--- src/test/pages/SignInPage.java
+++ src/test/pages/SignInPage.java
@@ -2,13 +2,22 @@ package test.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import test.data.User;
 
 public class SignInPage {
     private WebDriver driver;
 
-    private SignInPage(WebDriver driver) {
+    private By emailField = By.id("session_email");
+    private By passwordField = By.id("session_password");
+    private By commitButton = By.name("commit");
+
+    public SignInPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    public void signIn(User data) {
+        driver.findElement(emailField).sendKeys(data.getEmail());
+        driver.findElement(passwordField).sendKeys(data.getPassword());
+        driver.findElement(commitButton).click();
+    }
 }
-
