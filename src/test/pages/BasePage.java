430f9335d2201e029beb2ec328d84bac6b647173
Titus Fortner <titusfortner@gmail.com>
2018-04-30 19:54:29 -0500

    implement act-wait-act instead of wait-act

--- src/test/pages/BasePage.java
+++ src/test/pages/BasePage.java
@@ -1,6 +1,8 @@
 package test.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,13 +11,25 @@ public class BasePage {
     WebDriver driver;
 
     void click(By locator) {
-        waitForElement(locator);
-        driver.findElement(locator).click();
+        while (true) {
+            try {
+                driver.findElement(locator).click();
+                return;
+            } catch (NoSuchElementException | ElementNotVisibleException e) {
+                waitForElement(locator);
+            }
+        }
     }
 
     void sendKeys(By locator, String text) {
-        waitForElement(locator);
-        driver.findElement(locator).sendKeys(text);
+        while (true) {
+            try {
+                driver.findElement(locator).sendKeys(text);
+                return;
+            } catch (NoSuchElementException | ElementNotVisibleException e) {
+                waitForElement(locator);
+            }
+        }
     }
 
     private void waitForElement(By locator) {
