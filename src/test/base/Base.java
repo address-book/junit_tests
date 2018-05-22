e1b9bdf35a8c4514bffeb4d202ee7a014503a811
Titus Fortner <titusfortner@gmail.com>
2018-05-22 10:08:13 -0500

    Move common code to Super Class

---
+++ src/test/base/Base.java
@@ -0,0 +1,22 @@
+package test.base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+    protected WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "lib/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+}
