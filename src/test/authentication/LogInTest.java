2f43059c1259d2872ad5c76123f25c36fdf775f9
Titus Fortner <titusfortner@gmail.com>
2018-05-25 09:57:04 -0500

    Convert from imperative to declarative

--- src/test/authentication/LogInTest.java
+++ src/test/authentication/LogInTest.java
@@ -16,16 +16,12 @@ public class LogInTest extends Base {
     @Test
     public void signInSuccessfully() {
         HomePage homePage = HomePage.visit(driver);
-        homePage.getMenuButton().click();
-        SignInPage signInPage = homePage.getSignInLink().click();
+        homePage.navigateToSignIn();
 
-        signInPage.waitFor(homePage.getEmailElement);
-        User user = User.validUser();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.signIn(User.validUser());
 
-        signInPage.getEmailElement().sendKeys(user.getEmail());
-        signInPage.getPasswordElement().sendKeys(user.getPassword());
-        signInPage.getSubmitButton().click();
-        assertTrue(homePage.isElementPresent(homePage.getCurrentUser));
+        assertTrue(homePage.isSignedIn());
     }
 
     @Test
