9f9023b98b4e7b0be184652f23d70205e5babe9d
Titus Fortner <titusfortner@gmail.com>
2018-05-25 09:58:03 -0500

    Answers to exercise to write declarative test

--- src/test/authentication/LogInTest.java
+++ src/test/authentication/LogInTest.java
@@ -5,9 +5,6 @@ import test.data.*;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.Assert.*;
 
 public class LogInTest extends Base {
@@ -23,55 +20,18 @@ public class LogInTest extends Base {
 
     @Test
     public void signInUnsuccessfully() {
+        SignInPage signInPage = SignInPage.visit(driver);
+        signInPage.signInUnsuccessfully(User.invalidUser());
 
-        // Rewrite Test Declaratively
-
-        driver.get("http://a.testaddressbook.com");
-        driver.findElement(By.id("sign-in")).click();
-
-        Map<String, String> blankPassword = new HashMap<String, String>();
-        blankPassword.put("password", "");
-
-        User user = new User(blankPassword);
-        String email = user.getEmail();
-        String password = user.getPassword();
-
-        WebDriverWait explicitWait = new WebDriverWait(driver, 20);
-
-        WebElement emailElement = explicitWait.until(
-                ExpectedConditions.presenceOfElementLocated(By.id("session_email")));
-
-        emailElement.sendKeys(email);
-        driver.findElement(By.id("session_password")).sendKeys(password);
-        driver.findElement(By.tagName("form")).submit();
-
-        By emailField = By.id("sign-in");
-        assertTrue(driver.findElements(emailField).size() > 0);
+        assertTrue(signInPage.hasErrorMessage());
     }
 
     @Test
     public void signInBlankPassword() {
+        SignInPage signInPage = SignInPage.visit(driver);
+        signInPage.signInUnsuccessfully(User.blankPassword());
 
-        // Rewrite Test Declaratively
-
-        driver.get("http://a.testaddressbook.com");
-        driver.findElement(By.id("sign-in")).click();
-
-        User user = User.blankPassword();
-        String email = user.getEmail();
-        String password = user.getPassword();
-
-        WebDriverWait explicitWait = new WebDriverWait(driver, 20);
-
-        WebElement emailElement = explicitWait.until(
-                ExpectedConditions.presenceOfElementLocated(By.id("session_email")));
-
-        emailElement.sendKeys(email);
-        driver.findElement(By.id("session_password")).sendKeys(password);
-        driver.findElement(By.tagName("form")).submit();
-
-        By emailField = By.id("sign-in");
-        assertTrue(driver.findElements(emailField).size() > 0);
+        assertTrue(signInPage.hasErrorMessage());
     }
 
 }
