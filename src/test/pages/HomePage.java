6f2356c5472b77264a84032daac0eb98e975a8ae
Titus Fortner <titusfortner@gmail.com>
2018-05-25 09:58:49 -0500

    Write HomePage methods

---
+++ src/test/pages/HomePage.java
@@ -0,0 +1,25 @@
+package test.pages;
+
+import org.openqa.selenium.WebDriver;
+
+    public class HomePage {
+        private WebDriver driver;
+
+        public static HomePage visit(WebDriver driver) {
+            HomePage page = new HomePage(driver);
+            driver.get("http://a.testaddressbook.com");
+            return page;
+        }
+
+        private HomePage(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void navigateToSignIn() {
+            driver.findElement(signInLink).click();
+        }
+
+        public Boolean isSignedIn() {
+            return driver.findElements(user).size() > 0;
+        }
+    }
