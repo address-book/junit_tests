a8d236b98bc329e9c80635d0d7e5057fcf9bf21c
Titus Fortner <titusfortner@gmail.com>
2018-06-08 14:45:31 -0500

    Answers to exercise for implementing Address CRUD

--- src/test/exercises/UserJourney.java
+++ src/test/exercises/UserJourney.java
@@ -23,8 +23,8 @@ public class UserJourney extends Base {
 
         String username = "user@example.com";
         String password = "password";
-        String firstName = "";
-        String lastName = "";
+        String firstName = "Titus";
+        String lastName = "Fortner";
         String streetAddress = "123 Main";
         String secondaryAddress = "Apt 1";
         String city = "Somewhere";
@@ -40,25 +40,55 @@ public class UserJourney extends Base {
 
         // Navigate to Sign In Page
 
+        driver.findElement(By.cssSelector("[data-test=sign-in]")).click();
+
         // Submit Sign In Form
+
+        driver.findElement(By.cssSelector("[data-test=email]")).sendKeys(username);
+        driver.findElement(By.cssSelector("[data-test=password]")).sendKeys(password);
+        driver.findElement(By.cssSelector("[data-test=submit]")).click();
 
         // Navigate to Address List Page
 
+        driver.findElement(By.cssSelector("[data-test=addresses]")).click();
+
         // Navigate to New Address Page
+
+        driver.findElement(By.cssSelector("[data-test=create]")).click();
 
         // Fill Out New Address Form & Submit
 
             // First Name, Last Name, Street Address, Secondary Address & City
+        driver.findElement(By.id("address_first_name")).sendKeys(firstName);
+        driver.findElement(By.id("address_last_name")).sendKeys(lastName);
+        driver.findElement(By.id("address_street_address")).sendKeys(streetAddress    );
+        driver.findElement(By.id("address_secondary_address")).sendKeys(secondaryAddress);
+        driver.findElement(By.id("address_city")).sendKeys(city);
 
             // State (use Select class)
+        WebElement stateElement = driver.findElement(By.id("address_state"));
+        Select dropdown = new Select(stateElement);
+        dropdown.selectByValue(state);
 
             // Zip Code, Country, Birthday, Age, Website
+        driver.findElement(By.id("address_zip_code")).sendKeys(zipCode);
+        driver.findElement(By.id("address_country_true")).click();
+        driver.findElement(By.id("address_birthday")).sendKeys(birthday);
+        driver.findElement(By.id("address_age")).sendKeys(age);
+        driver.findElement(By.id("address_website")).sendKeys(website);
 
             // Upload File
+        WebElement inputElement = driver.findElement(By.id("address_picture"));
+        File file = new File(fileLocation);
+        inputElement.sendKeys(file.getCanonicalPath());
 
             // Phone, Interest, Note
+        driver.findElement(By.id("address_phone")).sendKeys(phone);
+        driver.findElement(By.id("address_interest_dance")).click();
+        driver.findElement(By.id("address_note")).sendKeys(note);
 
             // Submit Form
+        driver.findElement(By.cssSelector("[data-test=submit]")).click();
 
         // Get ID from URL
 
@@ -72,20 +102,44 @@ public class UserJourney extends Base {
 
         // Navigate to Edit Address Page
 
+        driver.findElement(By.cssSelector("[data-test=edit]")).click();
+
         // Edit the First Name with newName and Submit
 
         String newName = "Jack";
+        driver.findElement(By.id("address_first_name")).clear();
+        driver.findElement(By.id("address_first_name")).sendKeys(newName);
+        driver.findElement(By.cssSelector("[data-test=submit]")).click();
 
         // Navigate to Address List Page
 
+        driver.findElement(By.cssSelector("[data-test=list]")).click();
+
         // Get list of Destroy Link elements
+
+        List<WebElement> destroyLinks = driver.findElements(By.cssSelector("[data-test^=destroy]"));
 
         // Iterate over the list to click the one matching the id we found above
 
+        WebElement correctElement = null;
+
+        for(WebElement element : destroyLinks)
+        {
+            if(element.getAttribute("data-test").contains(id)) {
+                correctElement = element;
+                correctElement.click();
+                break;
+            }
+        }
+
         // Accept Alert
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
 
         // Wait for link to go away
 
+        Thread.sleep(5000);
     }
 
 }
