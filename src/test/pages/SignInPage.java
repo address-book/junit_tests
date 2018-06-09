c3350421d2b1592a26469c5f2366707b31fc4f4a
Titus Fortner <titusfortner@gmail.com>
2018-06-09 16:39:45 -0500

    Implemented SignInPage page object

--- src/test/pages/SignInPage.java
+++ src/test/pages/SignInPage.java
@@ -2,15 +2,41 @@ package test.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-// Implement Methods & Elements to allow LoginTest tests to pass
+import test.data.*;
 
 public class SignInPage {
     private WebDriver driver;
 
-    private SignInPage(WebDriver driver) {
+    private By emailField = By.id("session_email");
+    private By passwordField = By.id("session_password");
+    private By commitButton = By.name("commit");
+    private By error = By.className("alert-notice");
+
+    public static SignInPage visit(WebDriver driver) {
+        SignInPage page = new SignInPage(driver);
+        driver.get("http://a.testaddressbook.com/sign_in/");
+        return page;
+    }
+
+    public SignInPage(WebDriver driver) {
         this.driver = driver;
     }
 
-}
+    public void signIn(User data) {
+        fillForm(data);
+    }
 
+    public void signInUnsuccessfully(User data) {
+        fillForm(data);
+    }
+
+    public Boolean hasErrorMessage() {
+        return driver.findElements(error).size() > 0;
+    }
+
+    private void fillForm(User data) {
+        driver.findElement(emailField).sendKeys(data.getEmail());
+        driver.findElement(passwordField).sendKeys(data.getPassword());
+        driver.findElement(commitButton).click();
+    }
+}
