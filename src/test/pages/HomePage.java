ac3a679b96632131391393ba745f14710fef982d
Titus Fortner <titusfortner@gmail.com>
2018-06-09 14:59:38 -0500

    write HomePage elements

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -1,9 +1,12 @@
 package test.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
     public class HomePage {
         private WebDriver driver;
+
+        private By user = By.cssSelector("[data-test=current-user]");
 
         public HomePage(WebDriver driver) {
             this.driver = driver;
