89c9a74ae6a548c9c04e5aa6727cb7d7a84762ab
Titus Fortner <titusfortner@gmail.com>
2018-06-09 14:59:58 -0500

    Setup Exercise to implement SignInPage page object

---
+++ src/test/pages/SignInPage.java
@@ -0,0 +1,16 @@
+package test.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+// Implement Methods & Elements to allow LoginTest tests to pass
+
+public class SignInPage {
+    private WebDriver driver;
+
+    private SignInPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
+
